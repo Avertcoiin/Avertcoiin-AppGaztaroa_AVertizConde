4a2d56e281b4bf908c7a75348cf5d68c7bf731c9
Avertcoiin <vertiz.138863@e.unavarra.es>
2025-04-07 15:53:10 +0200

    Drawer Navigation

--- appGaztaroaJavascript/componentes/CampobaseComponent.js
+++ appGaztaroaJavascript/componentes/CampobaseComponent.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import Constants from 'expo-constants';
 import Calendario from './CalendarioComponent';
 import DetalleExcursion from './DetalleExcursionComponent';
-import { Platform, View, Text } from 'react-native';
+import { Platform, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { HeaderBackButton } from '@react-navigation/elements';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Home from './HomeComponent';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
+// StackNavigator para la pantalla "Calendario" y "DetalleExcursion"
 function CalendarioNavegador() {
   return (
     <Stack.Navigator
@@ -38,12 +41,59 @@ function CalendarioNavegador() {
   );
 }
 
+// StackNavigator para la pantalla "Home"
+function HomeNavegador() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headMode: 'screen',
+        headerTintColor: '#fff',
+        headerStyle: { backgroundColor: '#015afc' },
+        headerTitleStyle: { color: '#fff' },
+        headerTitleAlign: 'center',
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: 'Campo Base',
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+// DrawerNavigator que contiene los StackNavigators
+function DrawerNavegador() {
+  return (
+    <Drawer.Navigator
+      initialRouteName="Campo base"
+      screenOptions={{
+        headerShown: false, // Ocultar encabezado del DrawerNavigator
+        drawerStyle: { backgroundColor: '#c2d3da' },
+      }}
+    >
+      <Drawer.Screen
+        name="Campo base"
+        component={HomeNavegador}
+      />
+      <Drawer.Screen
+        name="Calendario"
+        component={CalendarioNavegador}
+      />
+    </Drawer.Navigator>
+  );
+}
+
+// Componente principal que envuelve todo en un único NavigationContainer
 class Campobase extends Component {
   render() {
     return (
       <NavigationContainer>
         <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
-          <CalendarioNavegador />
+          <DrawerNavegador />
         </View>
       </NavigationContainer>
     );
